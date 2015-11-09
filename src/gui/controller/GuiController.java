61ab5105ed1a51431d24b9a353e653f4f2d8ccf8
ghop1073 <mhop1073@740s-pl-109.local>
2015-11-09 12:19:04 -0700

    made new project to learn about gui

---
+++ src/gui/controller/GuiController.java
@@ -0,0 +1,19 @@
+package gui.controller;
+
+import gui.view.GuiFrame;
+
+public class GuiController
+{
+
+	private GuiFrame baseFrame;
+	
+	public GuiController()
+	{
+		baseFrame = new GuiFrame(this);
+	}
+	
+	public void start()
+	{
+		
+	}
+}
